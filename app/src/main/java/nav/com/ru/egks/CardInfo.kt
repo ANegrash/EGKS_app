876e4c3c4958b8982e41d4d021d7c9f6c907a457
Andrey Negrash <andi.ne2002@gmail.com>
2021-12-31 14:13:40 +0300

    cardInfo works

--- app/src/main/java/nav/com/ru/egks/CardInfo.kt
+++ app/src/main/java/nav/com/ru/egks/CardInfo.kt
@@ -2,9 +2,10 @@ package nav.com.ru.egks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import nav.com.ru.egks.models.CardInfoModel
@@ -17,12 +18,23 @@ class CardInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_info)
+        setFrameLayoutContent(0, 1, 0)
 
         var url = "https://nav-com.ru/egks?number="
         val intent = intent
         val cardNum = intent.getStringExtra("number")
-        val balTw = findViewById<TextView>(R.id.balance)
+        val cardName = intent.getStringExtra("name")
+        val balTw = findViewById<TextView>(R.id.bal)
+        val expTw = findViewById<TextView>(R.id.exp)
+        val toolbarText = findViewById<TextView>(R.id.custom_title)
         val cardImg = findViewById<ImageView>(R.id.cardImage)
+        val backBtn = findViewById<ImageView>(R.id.backButton)
+        toolbarText.text = cardName
+
+        backBtn.setOnClickListener {
+            finish()
+        }
+
         url += cardNum
 
         val getResponse = Get()
@@ -32,7 +44,7 @@ class CardInfo : AppCompatActivity() {
             object : Callback {
                 override fun onFailure(call: Call, e: IOException) {
                     runOnUiThread {
-                        Log.e("TAG", "ERROR:" + e)
+                        setFrameLayoutContent(0, 0, 1)
                     }
                 }
 
@@ -44,22 +56,49 @@ class CardInfo : AppCompatActivity() {
 
                         val cardInfo: CardInfoModel = gson.fromJson(stringResponse, CardInfoModel::class.java)
                         runOnUiThread {
-
-                            balTw.text = cardInfo.rub.toString() + "r " + cardInfo.cent.toString() + "c"
+                            supportActionBar?.hide()
+                            balTw.text = cardInfo.rub.toString() + "р. " + cardInfo.cent.toString() + "к."
+                            expTw.text = cardInfo.exp
 
                             Picasso.with(this@CardInfo)
                                 .load(cardInfo.img)
                                 .placeholder(R.drawable.card)
                                 .error(R.drawable.card)
                                 .into(cardImg)
+                            setFrameLayoutContent(1, 0, 0)
                         }
                     } else {
                         runOnUiThread {
-                            Log.e("TAG", "EMPTY BODY")
+                            setFrameLayoutContent(0, 0, 1)
                         }
                     }
                 }
             }
         )
+    }
+
+    private fun setFrameLayoutContent (
+        main: Int = 1,
+        loading: Int = 0,
+        error: Int = 0
+    ) {
+        val mainLayout = findViewById<ConstraintLayout>(R.id.main)
+        val loadingLayout = findViewById<ConstraintLayout>(R.id.loading)
+        val errorLayout = findViewById<ConstraintLayout>(R.id.error)
+
+        when (main) {
+            0 -> mainLayout.visibility = View.GONE
+            1 -> mainLayout.visibility = View.VISIBLE
+        }
+
+        when (loading) {
+            0 -> loadingLayout.visibility = View.GONE
+            1 -> loadingLayout.visibility = View.VISIBLE
+        }
+
+        when (error) {
+            0 -> errorLayout.visibility = View.GONE
+            1 -> errorLayout.visibility = View.VISIBLE
+        }
     }
 }
--- app/src/main/java/nav/com/ru/egks/models/CardInfoModel.kt
+++ app/src/main/java/nav/com/ru/egks/models/CardInfoModel.kt
@@ -3,5 +3,6 @@ package nav.com.ru.egks.models
 data class CardInfoModel (
     var rub: Any,
     var cent: Any,
-    var img: String
+    var img: String,
+    var exp: String
 )
